d74d5f48105abb9d0a17869de6ec895ccd027f4f
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-02-02 21:23:35 +0530

    minor changes

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import Icon from '../../assets/icon.png'
-import './navbar.css'
+import React from "react";
+import Icon from "../../assets/icon.png";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <div className='navbarr'>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <img className='icon' src={Icon} alt="" />
-                    <a className="navbar-brand brand" href="/">FitFuel</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            
-                        </ul>
-                        <a className="navbar-brand brandd" href="/">About Us</a>
-                        <form className="d-flex" role="search">
-                            <button className="btn btn-outline-primary btn-color" type="submit">Login</button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
+  return (
+    <div className="navbarr">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <img className="icon" src={Icon} alt="" />
+          <a className="navbar-brand brand" href="/">
+            FitFuel
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+            <a className="navbar-brand brandd" href="/">
+              About Us
+            </a>
+            <form className="d-flex" role="search">
+              <Link to="/login" className="btn btn-outline-primary btn-color">
+                Login
+              </Link>
+            </form>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
