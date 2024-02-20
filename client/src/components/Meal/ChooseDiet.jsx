c3116082076ae0d77279c2d74a259fe3d90b8106
“ankitaghadge19” <ghadge.ankita197@gmail.com>
2024-02-20 12:06:59 +0530

    successfully passed totalCalories state from ChooseDiet.jsx to VegMeal.jsx

--- client/src/components/Meal/ChooseDiet.jsx
+++ client/src/components/Meal/ChooseDiet.jsx
@@ -1,69 +1,166 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import veg from '../../assets/vegg.png'
-import nonveg from '../../assets/non.png'
-import egg from '../../assets/egg.png'
-import vegan from '../../assets/vegan.png'
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import veg from "../../assets/vegg.png";
+import nonveg from "../../assets/non.png";
+import egg from "../../assets/egg.png";
+import vegan from "../../assets/vegan.png";
 import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 const ChooseDiet = ({ totalCalories }) => {
-    console.log("Inside ChooseDiet.jsx: ", totalCalories);
-    return (
-        <div>
-            <Box>
-            <div style={{ background:'linear-gradient(to right, #4be643, #f6fc3a, #c46f6a, #f72d2d)', marginBottom:'-0.5rem', padding:'1rem'}}>
-                    <h1 style={{textAlign:'center', color:'black'}}>Get Your Meal Now!</h1>
-                </div>
-                <Grid container>
-                
-                    <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '2rem', background: 'linear-gradient(to bottom, #4be643, white)' }}>
-                        <Link style={{textDecoration:'none'}} to='/veg'>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                                <img src={veg} style={{ height: '15rem' }} alt="" />
-                                <button type="button" className="btn btn-success">Vegetarian</button>
-                            </Box>
-                        </Link>
+  const navigate = useNavigate();
 
-                    </Grid>
+  console.log("Inside ChooseDiet.jsx: ", totalCalories);
 
+  const navigateToVegMeal = () => {
+    // Use the navigate function to go to '/veg' with the state
+    navigate("/veg", { state: { totalCalories: totalCalories } });
+  };
 
-
-                    <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '2rem', background: 'linear-gradient(to bottom, #f6fc3a, white)' }}>
-                        <Link to='/' style={{textDecoration:'none'}}>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                                <img src={egg} style={{ height: '15rem' }} alt="" />
-                                <button type="button" className="btn btn-warning" style={{color:'white'}}>Egg</button>
-                            </Box>
-                        </Link>
-                    </Grid>
-
-                    <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '2rem', background: 'linear-gradient(to bottom, #c46f6a, white)' }}>
-                        <Link  style={{textDecoration:'none'}}>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                                <img src={vegan} style={{ height: '15rem' }} alt="" />
-                                <button type="button" className="btn " style={{ backgroundColor: '#c46f6a', color:'white' }}>Vegan</button>
-                            </Box>
-                        </Link>
-
-                    </Grid>
-                    <Grid item xs={6} md={3} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', padding: '2rem', background: 'linear-gradient(to bottom, #f72d2d, white)' }}>
-                        <Link style={{textDecoration:'none'}}>
-                            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                                <img src={nonveg} style={{ height: '13.5rem' }} alt="" />
-                                <br />
-                                <button type="button" className="btn btn-danger">Non Veg</button>
-                            </Box>
-                        </Link>
-
-                    </Grid>
-
-                </Grid>
-            </Box>
-            
+  return (
+    <div>
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(to right, #4be643, #f6fc3a, #c46f6a, #f72d2d)",
+            marginBottom: "-0.5rem",
+            padding: "1rem",
+          }}
+        >
+          <h1 style={{ textAlign: "center", color: "black" }}>
+            Get Your Meal Now!
+          </h1>
         </div>
-    )
-}
+        <Grid container>
+          <Grid
+            item
+            xs={6}
+            md={3}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "2rem",
+              background: "linear-gradient(to bottom, #4be643, white)",
+            }}
+          >
+            {/* Use Link to navigate to VegMeal and pass totalCalories as a prop */}
 
-export default ChooseDiet
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+              }}
+            >
+              <img src={veg} style={{ height: "15rem" }} alt="" />
+              <button
+                type="button"
+                className="btn btn-success"
+                onClick={navigateToVegMeal}
+              >
+                Vegetarian
+              </button>
+            </Box>
+          </Grid>
+
+          <Grid
+            item
+            xs={6}
+            md={3}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "2rem",
+              background: "linear-gradient(to bottom, #f6fc3a, white)",
+            }}
+          >
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <img src={egg} style={{ height: "15rem" }} alt="" />
+                <button
+                  type="button"
+                  className="btn btn-warning"
+                  style={{ color: "white" }}
+                >
+                  Egg
+                </button>
+              </Box>
+            </Link>
+          </Grid>
+
+          <Grid
+            item
+            xs={6}
+            md={3}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "2rem",
+              background: "linear-gradient(to bottom, #c46f6a, white)",
+            }}
+          >
+            <Link style={{ textDecoration: "none" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <img src={vegan} style={{ height: "15rem" }} alt="" />
+                <button
+                  type="button"
+                  className="btn "
+                  style={{ backgroundColor: "#c46f6a", color: "white" }}
+                >
+                  Vegan
+                </button>
+              </Box>
+            </Link>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            md={3}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              padding: "2rem",
+              background: "linear-gradient(to bottom, #f72d2d, white)",
+            }}
+          >
+            <Link style={{ textDecoration: "none" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                }}
+              >
+                <img src={nonveg} style={{ height: "13.5rem" }} alt="" />
+                <br />
+                <button type="button" className="btn btn-danger">
+                  Non Veg
+                </button>
+              </Box>
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+};
+
+export default ChooseDiet;
